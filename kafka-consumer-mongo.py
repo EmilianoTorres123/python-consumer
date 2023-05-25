9c1fb83a8d4fc609affa95f115660ed103dd1bec
emiliano <emiliontn@hotmail.com>
2023-05-25 03:55:32 -0600

    update python-consumer

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -9,8 +9,7 @@ from pymongo.server_api import ServerApi
 import json
 
 uri = 'mongodb+srv://leiferTorres:h7D7B1EhlJQxgub7@cluster0.07nwh.mongodb.net/dbapi?retryWrites=true&w=majority'
-
-
+#uri= 'mongodb://127.0.0.1:27017/memes'
 # Create a new client and connect to the server
 #client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
@@ -33,7 +32,31 @@ try:
 except:
     print("Could not connect to MongoDB")
 
-consumer = KafkaConsumer('test',bootstrap_servers=['my-kafka-0.my-kafka-headless.emilianotorres123.svc.cluster.local:9092'])
+consumer = KafkaConsumer('test',bootstrap_servers=[
+    'my-kafka-0.my-kafka-headless.emilianotorres123.svc.cluster.local:9092'
+    #'localhost:9092'
+    ])
+
+#count
+try:
+    agg_result=db.memes_info.aggregate(
+    [
+        {
+        "$group":
+        {
+            "_id":"$name",
+            "n":{"$sum":1}
+        }}
+    ])
+    db.memes_summary.delete_many({})
+    for i in agg_result:
+        print(i)
+        summary_id= db.memes_summary.insert_one(i)
+        print("Summary inserted  with records  ids",summary_id)
+except Exception as e:
+    print(f'Group by caught {type(e)}:')
+    print(e)
+
 # Parse received data from Kafka
 for msg in consumer:
     record = json.loads(msg.value)
